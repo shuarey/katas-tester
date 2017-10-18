0566790b9c0912950dcf5a6fd536757a3a884a6b
Joshua Reynvaan <31830510+shuarey@users.noreply.github.com>
2017-10-17 22:08:21 -0700

    Katas from Code Wars

--- user input/Program.cs
+++ user input/Program.cs
@@ -16,9 +16,64 @@ namespace kata_tester
             //int evenEval = Convert.ToInt32(Console.ReadLine());
             //EvenOrOdd(evenEval);
             //basicOp("a", 12, 0);
-            Console.WriteLine(string.Join(", ", CountPositivesSumNegatives(new int[] {  })));
+            /*
+             * TODO
+             * 
+             * Console.WriteLine(string.Join(", ", CountPositivesSumNegatives(new int[] {  })));
+             * 
+             */
+            //var myBmi = Bmi(183.4, 6.1);
+            //Console.WriteLine($"Based on your Bmi, you are: {myBmi}");
+            //var mySample = RemoveExclamationMarks("!!!!!!!!!!!!!!!!!!!!!!!!!!I !!!!!!!!!!!!!!!!!!!!!!am !!!!!!!!!!!!!!!!Batman");
+            //Console.WriteLine(mySample);
+            DoubleChar("Hello");
         }
 
+        public static string DoubleChar(string s)
+        {
+            char[] myCharArray = s.ToCharArray();
+            Console.WriteLine(myCharArray);
+            return "";
+        }
+
+        //public double[] Tribonacci(double[] signature, int n)
+        //{
+        //    double[] myArray = new double[n] { signature };
+        //    return double; 
+        //}
+
+        // Completed
+        public static string RemoveExclamationMarks(string s)
+        {
+            //string sample = s;
+            return s.Replace("!", "");
+        }
+
+        // Completed
+        public static string Bmi(double weight, double height)
+        {
+            var result = "";
+            var bmi = (weight / Math.Pow((height * 12), 2)) * 703;
+            if (bmi <= 18.5)
+            {
+                result = "Underweight";
+            }
+            else if (bmi <= 25.0)
+            {
+                result = "Normal";
+            }
+            else if (bmi <= 30.0)
+            {
+                result = "Overweight";
+            }
+            else if (bmi > 30.0)
+            {
+                result = "Obese";
+            }
+            return bmi + " " + result;
+        }
+
+        // TODO
         public static int[] CountPositivesSumNegatives(int[] input)
         {
             int negativeSum = 0, positiveCount = 0;
@@ -42,6 +97,7 @@ namespace kata_tester
             return new [] { positiveCount, negativeSum };
         }
 
+        // Completed
         public static double basicOp(string op, double val1, double val2)
         {
             string switchCase = op.ToString().ToUpper();
@@ -80,7 +136,8 @@ namespace kata_tester
             Console.WriteLine($"Result: {returnValue}");
             return returnValue;
         }
-            
+
+        // Completed    
         public static string EvenOrOdd(int number)
         {
             return (number % 2 == 0) ? "Even" : "Odd";
@@ -96,6 +153,7 @@ namespace kata_tester
             //}
         }
 
+        // Completed
         public static bool IsSquare(int n)
         {
             double myDouble = Math.Sqrt(n);
